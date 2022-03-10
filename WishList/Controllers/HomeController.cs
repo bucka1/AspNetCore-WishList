b15b0635b25acece9fa3b82d099715ea739c5c8b
Austin Buck <bucka1@michigan.gov>
2022-03-10 14:21:06 -0500

    Completed module

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Error()
+        {
+            return Error();
+        }
+        public IActionResult Index()
+        {
+            return Index();
+        }
+
+
+    }
+}
