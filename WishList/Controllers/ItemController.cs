588788b822d47e426dafaa016b67637c69840d9b
Austin Buck <bucka1@michigan.gov>
2022-03-11 08:28:40 -0500

    Completed module

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        
+
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+    
+        [HttpGet]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            foreach (var item in _context.Items)
+            {
+                if(item.Id == Id)
+                {
+                    _context.Remove(item);
+                }
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
